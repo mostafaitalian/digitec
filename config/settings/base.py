38e58cc267274e4e7e26e87481b7196629542377
mostafa mohamed yossef <39102220+mostafaitalian@users.noreply.github.com>
2021-05-14 15:25:01 +0200

    Update base.py

--- config/settings/base.py
+++ config/settings/base.py
@@ -243,7 +243,7 @@ EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT ='587'
 
 EMAIL_HOST_USER='mostafaitalian1379@gmail.com'
-EMAIL_HOST_PASSWORD = '100aa100'
+EMAIL_HOST_PASSWORD = ''
 DEFAULT_FROM_EMAIL = 'mostafaitalian1379@gmail.com'
 SERVER_EMAIL = 'eng_mustafa_yossef@hotmail.com'
 TWEET_APPROVER_EMAIL = 'eng_mustafa_yossef@hotmail.com'
